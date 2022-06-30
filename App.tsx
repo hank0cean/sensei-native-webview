8549a63d32885927614baa1b4dfb20ae88b63eba
hank0cean <coreyjake.hansen@gmail.com>
2022-06-30 10:53:29 -0700

    basic react-native-webview using sensei.pierasystems.com

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
+  const INJECTEDJAVASCRIPT = `const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+      <WebView
+        source={{
+          uri: 'https://sensei.pierasystems.com'
+        }}
+        // scalesPageToFit={isAndroid() ? false : true}
+        injectedJavaScript={INJECTEDJAVASCRIPT}
+        style={{ marginTop: 35 }}
+      />
+    // </View>
   );
 }
 
